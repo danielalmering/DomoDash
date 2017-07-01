9692a9580a4365ecbef29a77f2b7c8b6e6ff7897
danielalmering <danielalmering@sensemakers.nl>
2017-07-01 18:38:43 +0200

    Added all icons from domoticz to bind to fontawesome

--- switches/switches.controller.js
+++ switches/switches.controller.js
@@ -4,6 +4,7 @@ function SwitchesController($scope, $rootScope, $http, CONFIG, HOSTLOGIN) {
     vm.switches           = [];
     vm.switchDevice       = switchDevice;
     vm.render             = false;
+    vm.getIcon            = getIcon;
 
     activate();
 
@@ -32,6 +33,55 @@ function SwitchesController($scope, $rootScope, $http, CONFIG, HOSTLOGIN) {
         $http.get(CONFIG.HOSTNAME + '/json.htm?' + HOSTLOGIN + 'type=command&param=switchlight&idx=' + device.idx + '&switchcmd=' + status).then(function() {
             getSwitches();
         });
+    }
+
+    function getIcon(device){
+        if(device.SwitchType === 'Dimmer'
+        || device.SwitchType === 'Media Player'
+        || device.SwitchType === 'On/Off'
+        || device.SwitchType === 'Selector'){
+            switch (device.Image) {
+                case 'Light':         return 'fa-lightbulb-o'; break;
+                case 'Alarm':         return 'fa-bell'; break;
+                case 'Amplifier':     return 'fa-bullhorn'; break;
+                case 'ChristmasTree': return 'fa-tree'; break;
+                case 'Computer':      return 'fa-laptop'; break;
+                case 'ComputerPC':    return 'fa-desktop'; break;
+                case 'Cooling':       return 'fa-snowflake-o'; break;
+                case 'Fan':           return 'fa-refresh'; break;
+                case 'Fireplace':     return 'fa-fire'; break;
+                case 'Generic':       return 'fa-power-off'; break;
+                case 'Harddisk':      return 'fa-hdd-o'; break;
+                case 'Heating':       return 'fa-thermometer-full'; break;
+                case 'Media':         return 'fa-youtube-play'; break;
+                case 'Phone':         return 'fa-mobile'; break;
+                case 'Printer':       return 'fa-print'; break;
+                case 'Speaker':       return 'fa-volume-up'; break;
+                case 'TV':            return 'fa-television'; break;
+                case 'WallSocket':    return 'fa-plug'; break;
+                case 'Water':         return 'fa-tint'; break;
+                default:
+            }
+        } else {
+            switch (device.SwitchType) {
+                case 'Blinds':                      return 'fa-bars'; break;
+                case 'Blinds Inverted':             return 'fa-bars'; break;
+                case 'Blinds Percentage':           return 'fa-bars'; break;
+                case 'Blinds Percentage Inverted':  return 'fa-bars'; break;
+                case 'Contact':                     return 'fa-exchange'; break;
+                case 'Door Lock':                   return 'fa-lock'; break;
+                case 'Doorbell':                    return 'fa-bell-o'; break;
+                case 'Dusk Sensor':                 return 'fa-sun-o'; break;
+                case 'Motion Sensor':               return 'fa-assistive-listening-systems'; break;
+                case 'Push Off Button':             return 'fa-toggle-off'; break;
+                case 'Push On Button':              return 'fa-toggle-on'; break;
+                case 'Smoke Detector':              return 'fa-cloud'; break;
+                case 'Venetian Blinds EU':          return 'fa-bars'; break;
+                case 'Venetian Blinds US':          return 'fa-bars'; break;
+                case 'X10 Siren':                   return 'fa-bullhorn'; break;
+                default:
+            }
+        }
     }
 
     //// Update
