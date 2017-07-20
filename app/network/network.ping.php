e8c92281ee31e9082df2f1179256aef3821b2b79
daniel almering <danielalmering@sensemakers.nl>
2017-07-20 11:35:01 +0200

    Added networkdevices

--- app/network/network.ping.php
+++ app/network/network.ping.php
@@ -1,37 +1,25 @@
 <?php
 $file   = file_get_contents("../../config.json");
 $array  = json_decode($file, true)['network'];
-$ip =   "127.0.0.1";
-
-
 
 function ping($host, $port, $timeout) {
-   $tB = microtime(true);
-   $fP = fSockOpen($host, $port, $errno, $errstr, $timeout);
-   if (!$fP) { return "down"; } // if host unavailable
-   $tA = microtime(true);
-   return round((($tA - $tB) * 1000), 0)." ms"; // if host is up - show ping delay
-
+    $tB = microtime(true);
+    $fP = fSockOpen($host, $port, $errno, $errstr, $timeout);
+    if (!$fP) { return "down"; } // if host unavailable
+    $tA = microtime(true);
+    return round((($tA - $tB) * 1000), 0)." ms"; // if host is up - show ping delay
 }
-echo ping("192.168.192.49", 443, 10);
 
-// for($i = 0; $i < count($array); $i++){
-//     //system("ping -c 4 2>&1" . $array[$i]['ip'], $output);
-//     system("ping -c 4 -n 1" . $array[$i]['ip'], $output);
-//     //exec("ping -n 1 $array[$i]['ip']", $output);
-//     //exec("ping 2>&1" . $array[$i]['ip'], $output);
-//     //exec(sprintf('ping -c 1 -W 5 %s', escapeshellarg($ip)), $res, $rval);
-//     print_r($output);
-//
-//
-//     //$array[$i]['status'] = strpos($output[2], 'unreachable') == false ? 'green' : 'red';
-//
-//     //$output = null;
-//     //$array[$i]['status'] = 'red';
-// }
+for($i = 0; $i < count($array); $i++){
+    $ping = ping("tcp://" . $array[$i]['ip'], 80, 10);
+    if($ping == 'down'){
+        $array[$i]['status']    = 'red';
+    } else {
+        $array[$i]['status']    = 'green';
+        $array[$i]['ms']        = $ping;
+    }
+}
 
-
-
-//echo json_encode($array);
+echo json_encode($array);
 
 ?>
