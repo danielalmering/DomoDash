c5335868eb051458c89eff28584c323bd4be50b9
danielalmering <danielalmering@sensemakers.nl>
2017-08-16 22:10:18 +0200

    Added sabnzb fixes

--- app/sabnzb/sabnzb.controller.js
+++ app/sabnzb/sabnzb.controller.js
@@ -1,13 +1,14 @@
 function SabnzbController($scope, $rootScope, $http, CONFIG, $interval) {
 
     var vm                = this;
+    var hostname          = CONFIG.sabnzb_hostname + '?apikey=' + CONFIG.sabnzb_api + '&output=json';
     vm.title              = $scope.name;
     vm.job                = 'resume';
     vm.downloads          = {};
     vm.history            = {};
     vm.toggleDownload     = toggleDownload;
 
-    if(CONFIG.sabnzb_hostname && CONFIG.sabnzb_api && CONFIG.sabnzb_internalhostname){
+    if(CONFIG.sabnzb_hostname && CONFIG.sabnzb_api){
         activate();
     }
 
@@ -21,13 +22,13 @@ function SabnzbController($scope, $rootScope, $http, CONFIG, $interval) {
     //// Public interface
 
     function getDownloads(){
-        $http.get(getHost() + '&mode=queue').then(function(res) {
+        $http.get(hostname + '&mode=queue').then(function(res) {
             vm.downloads = res.data.queue;
         });
     }
 
     function getHistory(){
-        $http.get(getHost() + '&mode=history&limit=3').then(function(res) {
+        $http.get(hostname + '&mode=history&limit=3').then(function(res) {
             vm.history = res.data.history;
         });
     }
@@ -36,32 +37,21 @@ function SabnzbController($scope, $rootScope, $http, CONFIG, $interval) {
         if(status === 'Downloading'){ vm.job = 'pause'; }
         if(status === 'Pause' || status === 'Idle'){ vm.job = 'resume'; }
         console.log(vm.job);
-        $http.get(getHost() + '&mode=queue&name=' + vm.job + '&value=' + id).then(function(res) {
+        $http.get(hostname + '&mode=queue&name=' + vm.job + '&value=' + id).then(function(res) {
             getDownloads();
         });
     }
 
-    function getHost(){
-        if(CONFIG.sabnzb_hostname.indexOf(location.hostname) != -1 || location.hostname === 'localhost') {
-            hostname = CONFIG.sabnzb_hostname + '?apikey=' + CONFIG.sabnzb_api + '&output=json';
-        } else {
-            hostname = CONFIG.sabnzb_internalhostname + '?apikey=' + CONFIG.sabnzb_api + '&output=json';
-        }
-
-        return hostname;
-    }
-
-
     //// Update
 
     var timepolling = $interval(function() {
-        if(CONFIG.sabnzb_hostname && CONFIG.sabnzb_api && CONFIG.sabnzb_internalhostname){
+        if(CONFIG.sabnzb_hostname && CONFIG.sabnzb_api){
             getDownloads();
         }
     }, 5000);
 
     $rootScope.$on('$reload', function (event, data) {
-        if(CONFIG.sabnzb_hostname && CONFIG.sabnzb_api && CONFIG.sabnzb_internalhostname){
+        if(CONFIG.sabnzb_hostname && CONFIG.sabnzb_api){
             getHistory();
         }
     });
